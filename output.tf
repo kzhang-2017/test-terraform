ed317d3127c4025c3f9e016feb3e2306f6153194
Kevin Zhang <116036437+kzhang-2017@users.noreply.github.com>
2025-05-28 11:45:28 -0400

    Update outputs with new variables

--- output.tf
+++ output.tf
@@ -1,3 +1,16 @@
 output "template_file" {
     value = data.template_file.test.rendered
 }
+
+output a {
+    description = "output of variable named a"
+    value = var.a
+}
+output b {
+    description = "output of variable named b"
+    value = var.b
+}
+output c {
+    description = "output of variable named c"
+    value = var.c
+}
