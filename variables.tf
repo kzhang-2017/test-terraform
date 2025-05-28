c40b5390953279daeaaff0f3f6947a4d97b847ef
Kevin Zhang <116036437+kzhang-2017@users.noreply.github.com>
2025-05-28 11:45:02 -0400

    Add more test variables

--- variables.tf
+++ variables.tf
@@ -4,3 +4,19 @@ variable "name" {
   description = "The name to pass to the template."
   default     = "world"
 }
+
+variable "a" {
+  description = "A simple string type variable with a default value."
+  type        = string
+  default     = "A"
+}
+variable "b" {
+  description = "A simple string type variable with a default value."
+  type        = string
+  default     = "B"
+}
+variable "c" {
+  description = "A simple string type variable with a default value."
+  type        = string
+  default     = "C"
+}
